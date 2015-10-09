1e24b4f905d949ce7c9d46f235cf7eb43cc5ecb2
James Myers <yesmantoozu2@hotmail.com>
2015-10-09 16:48:03 -0700

    Jquery fixed

--- js/main.js
+++ js/main.js
@@ -39,26 +39,37 @@ var nextPrompt = function() {
 
 //puts user answers into HTML
 var showFinal = function() {
-  $('.prompt').html('
-  	This is the story of <span class="fill">'+answers[0]+'</span>
-  	 and the <span class="fill">'+answers[1]+'</span>
-  	  <span class="fill">'+answers[2]+'</span>
-  	 .<span class="fill">'+answers[3]+'</span>
-  	  met his friend <span class="fill">'+answers[4]+'</span>
-  	  .<span class="fill">'+answers[5]+'</span>
-  	  ,they walked to the<span class="fill">'+answers[6]+'</span>
-  	  . They felt Hungry and went to the locan restaurant<span class="fill">'+answers[7]+'</span>
-  	  .They ordered a large plate of <span class="fill">'+answers[8]+'</span>
-  	  .<span class="fill">'+answers[9]+'</span>
-  	  <span class="fill">'+answers[10]+'</span>
-  	  <span class="fill">'+answers[11]+'</span>
-  	  <span class="fill">'+answers[12]+'</span>
-  	  <span class="fill">'+answers[13]+'</span>
-  	  <span class="fill">'+answers[14]+'</span>
-  	  <span class="fill">'+answers[15]+'</span>
-  	  <span class="fill">'+answers[16]+'</span>
-
-  	  ');
+  $('.prompt').html(
+  'This is the story of<span class="fill">'
+  + answers[0]
+  + '</span>and the <span class="fill">'
+  + answers[1] 
+  + '</span> <span class="fill">'
+  +answers[2]
+  +'</span> . <span class="fill">'
+  +answers[3]
+  + '</span> met his friend <span class="fill">'
+  +answers[4]
+  +'</span> . <span class="fill">'
+  +answers[5]+
+  '</span> ,they walked to the<span class="fill">'
+  +answers[6]
+  +'</span> . They felt Hungry and went to the locan restaurant<span class="fill">'
+  +answers[7]
+  +'</span>.They ordered a large plate of <span class="fill">'
+  +answers[8]
+  +'</span>.<span class="fill">'
+  +answers[9]+'</span> <span class="fill">'
+  +answers[10]
+  +'</span> <span class="fill">'
+  +answers[11]
+  +'</span> <span class="fill">'
+  +answers[12]+'</span> <span class="fill">'
+  +answers[13]+'</span> <span class="fill">'
+  +answers[14]+'</span> <span class="fill">'
+  +answers[15]+'</span> <span class="fill">'
+  +answers[16]+'</span>'
+  );
   
   $('button').hide();
   
@@ -67,6 +78,11 @@ var showFinal = function() {
 $('button').click(function() {
 	nextPrompt();
 });
+
+$('#reload').on('click', function() {
+	// window.location.href=window.location.href;
+	document.location.reload(true);
+})
 
 // Show the first prompt as soon as js loads
 nextPrompt();
