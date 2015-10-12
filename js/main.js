0befcfae37e7d4638a58cd8dbe2a10d491f0225c
James Myers <yesmantoozu2@hotmail.com>
2015-10-12 10:00:01 -0700

    adding more lines

--- js/main.js
+++ js/main.js
@@ -5,12 +5,40 @@ var prompts = [
 	'Type 1st name',
 	'Type an adjective',
 	'Type a noun',
-  	'Type an adjective',
   	'Type 1st name',
   	'Type 2nd name',
   	'Type Place',
  	'Type Restaurants name',
  	'Type favorite food',
+ 	'Type an adjective',
+ 	'Famous Celebrity',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
+ 	'',
    ];
 
 var answers=[];
@@ -42,9 +70,9 @@ var showFinal = function() {
   $('.prompt').html(
   'This is the story of<span class="fill">'
   + answers[0]
-  + '</span>and the <span class="fill">'
-  + answers[1] 
-  + '</span> <span class="fill">'
+  +'</span>and the <span class="fill">'
+  +answers[1] 
+  +'</span> <span class="fill">'
   +answers[2]
   +'</span> . <span class="fill">'
   +answers[3]
@@ -52,23 +80,67 @@ var showFinal = function() {
   +answers[4]
   +'</span> . <span class="fill">'
   +answers[5]+
-  '</span> ,they walked to the<span class="fill">'
+  '</span> , they walked to the<span class="fill">'
   +answers[6]
-  +'</span> . They felt Hungry and went to the locan restaurant<span class="fill">'
+  +'</span> . They felt Hungry and went to the local restaurant<span class="fill">'
   +answers[7]
-  +'</span>.They ordered a large plate of <span class="fill">'
+  +'</span>. They ordered a large plate of <span class="fill">'
   +answers[8]
-  +'</span>.<span class="fill">'
-  +answers[9]+'</span> <span class="fill">'
-  +answers[10]
+  +'</span>. At the restaurant, the famous <span class="fill">'
+  +answers[9]
   +'</span> <span class="fill">'
+  +answers[10]
+  +'</span> was sitting next to them.<span class="fill">'
   +answers[11]
   +'</span> <span class="fill">'
-  +answers[12]+'</span> <span class="fill">'
-  +answers[13]+'</span> <span class="fill">'
-  +answers[14]+'</span> <span class="fill">'
-  +answers[15]+'</span> <span class="fill">'
-  +answers[16]+'</span>'
+  +answers[12]
+  +'</span> <span class="fill">'
+  +answers[13]
+  +'</span> <span class="fill">'
+  +answers[14]
+  +'</span> <span class="fill">'
+  +answers[15]
+  +'</span> <span class="fill">'
+  +answers[16]
+  +'</span> <span class="fill">'
+  +answers[17]
+  +'</span> <span class="fill">'
+  +answers[18]
+  +'</span> <span class="fill">'
+  +answers[19]
+  +'</span> <span class="fill">'
+  +answers[20]
+  +'</span> <span class="fill">'
+  +answers[21]
+  +'</span> <span class="fill">'
+  +answers[22]
+  +'</span> <span class="fill">'
+  +answers[23]
+  +'</span> <span class="fill">'
+  +answers[24]
+  +'</span> <span class="fill">'
+  +answers[25]
+  +'</span> <span class="fill">'
+  +answers[26]
+  +'</span> <span class="fill">'
+  +answers[27]
+  +'</span> <span class="fill">'
+  +answers[28]
+  +'</span> <span class="fill">'
+  +answers[29]
+  +'</span> <span class="fill">'
+  +answers[30]
+  +'</span> <span class="fill">'
+  +answers[31]
+  +'</span> <span class="fill">'
+  +answers[32]
+  +'</span> <span class="fill">'
+  +answers[33]
+  +'</span> <span class="fill">'
+  +answers[34]
+  +'</span> <span class="fill">'
+  +answers[35]
+
   );
   
   $('button').hide();
