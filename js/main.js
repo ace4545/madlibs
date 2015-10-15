dd3f531f3ed8a1c6e0b545802ffa31f3bd15689f
James Myers <yesmantoozu2@hotmail.com>
2015-10-15 13:07:35 -0700

    finished story

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ console.log("JAVASCRIPT Loaded!")
 var prompts = [
 	'Type 1st name',
 	'Type an adjective',
-	'Type a noun',
-  	'Type 1st name',
-  	'Type 2nd name',
-  	'Type Place',
+	'Type the word adventure',
+  'Type 1st name',
+	'Type 2nd name',
+  'Type Place',
  	"Type Restaurant's name",
  	'Type favorite food',
  	'Type an adjective',
@@ -17,37 +17,24 @@ var prompts = [
  	'Type adjective',
  	'Type adjective',
  	'Type adjective',
+ 	'Type 1st Name',
+ 	'Type 1st Name',
+ 	'Type adjective',
+ 	'Type adjective',
+ 	'Type adjective',
+ 	'Type adjective',
+ 	'Type 1st Name',
+ 	'Type Random female person in room',
+ 	'Type same female person in room',
+ 	'Type Place',
+ 	'Type plural noun',
+ 	'Type 1st name',
+ 	'Type verb ending with ing',
+ 	'Type adjective',
  	'Type',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
- 	'',
+ 	'Type',
+ 	'Type',
+  'Type',
    ];
 
 var answers=[];
@@ -81,74 +68,69 @@ var showFinal = function() {
   + answers[0]
   +'</span>and the <span class="fill">'
   +answers[1] 
-  +'</span> <span class="fill">'
+  +'</span> . <span class="fill">'
   +answers[2]
-  +'</span> . <span class="fill">'
+  +'</span> met his friend <span class="fill">'
   +answers[3]
-  + '</span> met his friend <span class="fill">'
-  +answers[4]
   +'</span> . <span class="fill">'
+  +answers[4]
+  +'</span>  , they walked to the<span class="fill">'
   +answers[5]
-  +'</span> , they walked to the<span class="fill">'
-  +answers[6]
   +'</span> . They felt Hungry and went to the local restaurant<span class="fill">'
+  +answers[6]
+  +'</span> . They ordered a large plate of <span class="fill">'
   +answers[7]
-  +'</span>. They ordered a large plate of <span class="fill">'
+  +'</span> . At the restaurant, the famous <span class="fill">'
   +answers[8]
-  +'</span>. At the restaurant, the famous <span class="fill">'
+  +'</span>was sitting next to them.<span class="fill">'
   +answers[9]
-  +'</span> <span class="fill">'
-  +answers[10]
-  +'</span> was sitting next to them.<span class="fill">'
-  +answers[11]
-  +'</span> <span class="fill">'
-  +answers[12]
   +'</span> got up, asked for the manager, then left. <span class="fill">'
-  +answers[13]
+  +answers[10]
   +'</span> Later, after finishing their <span class="fill">'
-  +answers[14]
+  +answers[11]
   +'</span> meal, they left to go to the Spy museum. The Museum was the only <span class="fill">'
-  +answers[15]
+  +answers[12]
   +'</span> museum to hold the <span class="fill">'
+  +answers[13]
+  +'</span> gadgets of the spy world. They left the museum after a couple of hours. Next morning, <span class="fill">'
+  +answers[14]
+  +'</span> had to go to work. <span class="fill">'
+  +answers[15]
+  +'</span> worked for a company in a big building where he was Employee 427.None of this mattered because the narrator was off on a tangent. Now, this<span class="fill">'
   +answers[16]
-  +'</span> gadgets of the spy world. <span class="fill">'
+  +'</span>story is everywhere because the <span class="fill">'
   +answers[17]
-  +'</span> <span class="fill">'
+  +'</span>narrator is crazy. <span class="fill">'
   +answers[18]
-  +'</span> <span class="fill">'
+  +'</span> was too drunk to realize she in in this story.<span class="fill">'
   +answers[19]
-  +'</span> <span class="fill">'
+  +'</span> wanted to go to<span class="fill">'
   +answers[20]
-  +'</span> <span class="fill">'
+  +'</span>with her friends the <span class="fill">'
   +answers[21]
-  +'</span> <span class="fill">'
+  +'</span>. The students in WDI DTLA 6 were all having a good time because it was Friday and everyone finished their projects on time. Soon however, <span class="fill">'
   +answers[22]
-  +'</span> <span class="fill">'
+  +'</span> must leave and go back to his hum drum life of coding. The games were<span class="fill">'
   +answers[23]
-  +'</span> <span class="fill">'
+  +'</span> and the beer was gone.<span class="fill">'
   +answers[24]
-  +'</span> <span class="fill">'
+  +'</span> ,<span class="fill">'
   +answers[25]
-  +'</span> <span class="fill">'
+  +'</span> ,<span class="fill">'
   +answers[26]
-  +'</span> <span class="fill">'
+  +'</span> , and<span class="fill">'
   +answers[27]
-  +'</span> <span class="fill">'
+  +'</span> were all too<span class="fill">'
   +answers[28]
-  +'</span> <span class="fill">'
+  +'</span> to care about the<span class="fill">'
   +answers[29]
-  +'</span> <span class="fill">'
+  +'</span> games that were going on. Soon the <span class="fill">'
   +answers[30]
-  +'</span> <span class="fill">'
+  +'</span> day would be over. Soon the<span class="fill">'
   +answers[31]
-  +'</span> <span class="fill">'
+  +'</span>  would be gone and the <span class="fill">'
   +answers[32]
-  +'</span> <span class="fill">'
-  +answers[33]
-  +'</span> <span class="fill">'
-  +answers[34]
-  +'</span> <span class="fill">'
-  +answers[35]
+  +'</span>  building would be empty. THE END.This story was though up by James Michael Myers, and finished by all of you.<span class="fill">'
 
   );
   
